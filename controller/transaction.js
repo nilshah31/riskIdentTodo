404a28f960341311c46f501f0fe48cffb80caf07
Nil Shah <nilshah.31@gmail.com>
2019-06-20 08:24:07 +0530

    Modifes transcation controller and adds error codes

--- controller/transaction.js
+++ controller/transaction.js
@@ -1,37 +1,43 @@
 const errorCodes = require('../helpers/error-codes');
-const testData   = require('../db/test-data.json');
+const testData = require('../db/test-data.json');
 
 module.exports = {
-  getTransactions : function(req,res){
-    if(!req.query.transactionId || !req.query.confidenceLevel) return res.status(400).send(errorCodes.MISSING_PARAMETER)
-    res.status(200).send(findTransaction(req.query.transactionId,testData[0], req.query.confidenceLevel));
-  } 
+  getTransactions: function (req, res) {
+    let transactions = []
+    let node;
+    if (!req.query.transactionId || !req.query.confidenceLevel) return res.status(400).send(errorCodes.MISSING_PARAMETER);
+    node = findTransaction(req.query.transactionId, testData[0], req.query.confidenceLevel)
+    if(node) transactions = getManipulatedTransaction() 
+    res.status(200).send();
+  }
+}
+
+function getManipulatedTransaction(id, currentNode){
+
 }
 
 function findTransaction(id, currentNode, confidenceLevel) {
   var i,
-      currentChild,
-      result;
+    currentChild,
+    result;
 
-  if (id == currentNode.id) {
-      currentNode['connectionInfo'] = undefined;
-      return currentNode;
+  if (id == currentNode.id) { //should add confidenceLevel condition
+    currentNode['connectionInfo'] = undefined;
+    return currentNode;
   } else {
-      // Use a for loop instead of forEach to avoid nested functions
-      // Otherwise "return" will not work properly
-      
-        for (i = 0; i < currentNode.children.length || 0; i += 1) {
-          currentChild = currentNode.children[i];
+    // Use a for loop instead of forEach to avoid nested functions
+    // Otherwise "return" will not work properly
+    for (i = 0; i < (currentNode.children && currentNode.children.length) || 0; i += 1) {
+      currentChild = currentNode.children[i];
 
-          // Search in the current child
-          result = findTransaction(id, currentChild, confidenceLevel);
-          // Return the result if the node has been found
-          if (result !== false) {
-              return result;
-          }
-        }
-      
-      // The node has not been found and we have no more options
-      return false;
+      // Search in the current child
+      result = findTransaction(id, currentChild, confidenceLevel);
+      // Return the result if the node has been found
+      if (result !== false) {
+        return result;
+      }
+    }
+    // The node has not been found and we have no more options
+    return errorCodes.NOT_FOUND;
   }
 }
--- helpers/error-codes.js
+++ helpers/error-codes.js
@@ -2,5 +2,9 @@ module.exports = {
   MISSING_PARAMETER : {
     code : 'missing_parameter',
     body : 'Please make sure you are attaching all the required paramters'
+  },
+  NOT_FOUND : {
+    code : 'not_found',
+    body : 'Required transcation could not found, please try again with valid transcation ID'
   }
 }
