4f81ee8821166c11117ec521117a3f7508b5803c
Nil Shah <nil@zuru.tech>
2019-06-20 11:48:13 +0530

    Completed pop transactions

--- controller/transaction.js
+++ controller/transaction.js
@@ -1,43 +1,49 @@
 const errorCodes = require('../helpers/error-codes');
 const testData = require('../db/test-data.json');
-
+const _ = require('lodash');
 module.exports = {
   getTransactions: function (req, res) {
-    let transactions = []
+    let transactions;
     let node;
     if (!req.query.transactionId || !req.query.confidenceLevel) return res.status(400).send(errorCodes.MISSING_PARAMETER);
     node = findTransaction(req.query.transactionId, testData[0], req.query.confidenceLevel)
-    if(node) transactions = getManipulatedTransaction() 
-    res.status(200).send();
+    console.log(node)
+    if(node) transactions = popTransactions(node) 
+    res.status(200).send(transactions);
   }
 }
 
-function getManipulatedTransaction(id, currentNode){
-
+function popTransactions(currentNode){
+  var i,
+  currentChild,
+  allTranscations = [];  
+  let shallow = _.clone(currentNode)
+  shallow.children ? shallow.children=undefined : '';
+  allTranscations.push(shallow)
+  for (i = 0; i < (currentNode.children && currentNode.children.length) || 0; i += 1) {
+    currentChild = currentNode.children[i];
+    if(!currentChild.children) allTranscations.push(currentChild)
+    popTransactions(currentChild);
+  }
+  return allTranscations;
 }
 
 function findTransaction(id, currentNode, confidenceLevel) {
   var i,
     currentChild,
     result;
-
   if (id == currentNode.id) { //should add confidenceLevel condition
     currentNode['connectionInfo'] = undefined;
     return currentNode;
   } else {
-    // Use a for loop instead of forEach to avoid nested functions
-    // Otherwise "return" will not work properly
     for (i = 0; i < (currentNode.children && currentNode.children.length) || 0; i += 1) {
       currentChild = currentNode.children[i];
-
-      // Search in the current child
       result = findTransaction(id, currentChild, confidenceLevel);
       // Return the result if the node has been found
       if (result !== false) {
         return result;
       }
     }
-    // The node has not been found and we have no more options
     return errorCodes.NOT_FOUND;
   }
 }
